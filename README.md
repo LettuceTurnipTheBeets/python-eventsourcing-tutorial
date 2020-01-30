8fb4f2a1e898a2c62dae40c6277055cbdda72717
Zarek <zarek@zarekr.com>
2020-01-30 17:23:05 +0000

    ready for action

---
+++ README.md
@@ -0,0 +1,8 @@
+## About
+
+Python's eventsourcing library revolves around using domain events as a persistence mechanism in event sourced domain driven applications. In the article accompanying this code we take our first steps into building an event sourced domain driven application using the library by adapting our pure Python domain model to use the library's domain modelling tools. 
+
+## Companion code
+
+This is the companion code to https://www.eventsorcery.com/posts/building-a-real-world-domain-model-python/.
+
--- version_6.py
+++ version_6.py
@@ -2,14 +2,13 @@
 This is part of the code accompanying this article: 
 
 
-Version 5 introduces the eventsourcing library and makes some minor
-changes to demonstrate how little time it can take to integrate
-it into our pure domain model.  
+Version 6 fully updates our pure domain model to take advantage 
+of the eventsourcing library's domain modelling tools.
 """
-from uuid import uuid4
-from typing import Any
+from typing import Any, List
 from datetime import datetime
 from eventsourcing.domain.model.aggregate import AggregateRoot
+from eventsourcing.domain.model.decorators import attribute
 
 def get_simple_repr(obj):
     attributes = ("%s=%r" % (k, v) for k, v in obj.__dict__.items())
@@ -63,17 +62,24 @@ class Shares:
         return get_simple_repr(self)
 
 class Company(AggregateRoot):
+    __subclassevents__ = True
+
     def __init__(self, name: str, **kwargs: Any) -> None:
         super().__init__(**kwargs)
-        # We don't need this anymore: 
-        # self.id = uuid4()
-        self.name = name
+        self._name = name
         self.shareholders = []
         self.share_classes = []
-        self.events = []
+        # This is no longer necessary: 
+        # self.events = []
 
     def __repr__(self):
-        return f"Company(name='{self.name}', len(share_classes)={len(self.share_classes)}, len(events)={len(self.events)})"
+        # We need to remove reference to self.events here too:
+        # return f"Company(name='{self.name}', len(share_classes)={len(self.share_classes)}, len(events)={len(self.events)})"
+        return f"Company(name='{self.name}')"
+
+    @attribute
+    def name(self):
+        pass
 
     def create_share_class(
             self,
@@ -84,7 +90,12 @@ class Company(AggregateRoot):
             votes_per_share: int = 1,
             redeemable: bool = False
         ) -> None: 
-            event = self.ShareClassWasCreated(
+            # We don't need to specify the event ourselves anymore:
+            # event = self.ShareClassWasCreated(
+            # Instead, we use this method, passing the name of 
+            # the event to be triggered as the first argument:
+            self.__trigger_event__(
+                Company.ShareClassWasCreated,
                 name=name,
                 nominal_value=nominal_value,
                 entitled_to_dividends=entitled_to_dividends,
@@ -92,26 +103,38 @@ class Company(AggregateRoot):
                 votes_per_share=votes_per_share,
                 redeemable=redeemable
             )
-            event.apply(self)
-            self.events.append(event)
+            # We also no longer need to manually apply the changes:
+            # event.apply(self)
+            # The list of events is updated once the triggered event
+            # completes its mutations, so we don't need this either: 
+            # self.events.append(event)
 
-    class ShareClassWasCreated:
-        def __init__(self, **kwargs: Any) -> None:
-            self.__dict__['created_on'] = f"{datetime.now():%Y-%m-%d at %H:%M:%S%z}"
-            self.__dict__.update(kwargs)
-        
-        def apply(event, company):
+    class Event(AggregateRoot.Event):
+        pass
+
+    class ShareClassWasCreated(Event):
+        # def __init__(self, **kwargs: Any) -> None:
+        #     # We don't need this anymore
+        #     # self.__dict__['created_on'] = f"{datetime.now():%Y-%m-%d at %H:%M:%S%z}"
+        #     self.__dict__.update(kwargs)
+
+        # The library uses the term 'mutate' instead of 'apply':
+        # def apply(event, company):
+        def mutate(event, company):
             new_shareclass = ShareClass(
                 name=event.name, 
                 nominal_value=event.nominal_value, 
                 votes_per_share=event.votes_per_share
             )
             company.share_classes.append(new_shareclass)
+            # We don't need this anymore either:
+            # company.events.append(event)
 
-        def __repr__(self):
-            return "ShareClassWasCreated(" + ', '.join(
-                    "{0}={1!r}".format(*item) for item in self.__dict__.items()
-                ) + ')'
+        # We don't need this either as the library now stores the topic in the event
+        # def __repr__(self):
+        #     return "ShareClassWasCreated(" + ', '.join(
+        #             "{0}={1!r}".format(*item) for item in self.__dict__.items()
+        #         ) + ')'
 
     def add_new_shareholder(
         self,
@@ -119,20 +142,24 @@ class Company(AggregateRoot):
         share_class: ShareClass,
         number_of_shares: int
     ) -> None: 
-        event = self.NewShareholderWasAdded(
+        # event = self.NewShareholderWasAdded(
+        self.__trigger_event__(
+            Company.NewShareholderWasAdded,
             shareholder_name=shareholder_name,
             share_class=share_class,
             number_of_shares=number_of_shares
         )
-        event.apply(self)
-        self.events.append(event)
+        # event.apply(self)
+        # self.events.append(event)
 
-    class NewShareholderWasAdded:
-        def __init__(self, **kwargs: Any) -> None:
-            self.__dict__['added_on'] = f"{datetime.now():%Y-%m-%d at %H:%M:%S%z}"
-            self.__dict__.update(kwargs)
+    class NewShareholderWasAdded(Event):
+        # def __init__(self, **kwargs: Any) -> None:
+            # We don't need this either
+            # self.__dict__['added_on'] = f"{datetime.now():%Y-%m-%d at %H:%M:%S%z}"
+            # self.__dict__.update(kwargs)
         
-        def apply(event, company):
+        #def apply(event, company):
+        def mutate(event, company):
             new_shares = Shares(
                 number=event.number_of_shares, 
                 share_class=event.share_class
@@ -140,17 +167,32 @@ class Company(AggregateRoot):
             new_shareholder = Shareholder(name=event.shareholder_name)
             new_shareholder.shares_held.append(new_shares)
             company.shareholders.append(new_shareholder)
+            # We don't need this either
+            # company.events.append(event)
 
-        def __repr__(self):
-            return "NewShareholderWasAdded(" + ', '.join(
-                    "{0}={1!r}".format(*item) for item in self.__dict__.items()
-                ) + ')'
+        # We don't need this either as the library now stores the topic in the event
+        # def __repr__(self):
+        #     return "NewShareholderWasAdded(" + ', '.join(
+        #             "{0}={1!r}".format(*item) for item in self.__dict__.items()
+        #         ) + ')'
 
+events = []
 
-def test_version_5():
+def add_to_event_record(event: Company.Event) -> None:
+    events.extend(event)
+
+def is_company_event(event: Any) -> bool: 
+    return all(isinstance(e, Company.Event) for e in events)
+
+def test_version_6():
+    # Get subscriber ready
+    from eventsourcing.domain.model.events import subscribe
+    global events
+    subscribe(handler=add_to_event_record, predicate=is_company_event)
+
+    # Set up 
     new_company = Company.__create__(name="Rocinante Limited")
     new_company.create_share_class(name="ordinary", nominal_value=0.0001)
-    
     initial_shareholders = ["James Holden", "Amos Burton", "Naomi Nagata", "Alex Kamal"]
     for person in initial_shareholders:
         new_company.add_new_shareholder(
@@ -158,6 +200,7 @@ def test_version_5():
             number_of_shares = 2500,
             share_class = new_company.share_classes[0]
         )
+    new_company.__save__()
 
     # Check that the company has a newly created ordinary share class
     assert new_company.share_classes[0].name == "ordinary"
@@ -173,19 +216,17 @@ def test_version_5():
     assert new_company.shareholders[0].shares_held[0].share_class.name == "ordinary"
     assert new_company.shareholders[0].shares_held[0].share_class.nominal_value == 0.0001
 
-    # Check that we have something useful in our new event log
-    assert len(new_company.events) == 5
-    assert isinstance(new_company.events[0], Company.ShareClassWasCreated)
-    assert hasattr(new_company.events[0], "created_on")
-    isinstance(new_company.events[1], Company.NewShareholderWasAdded)
-    assert new_company.events[1].shareholder_name == "James Holden"
+    # Check that our event log has some stuff in it
+    assert len(events) == 6
 
     return new_company
 
 if __name__ == "__main__":
-    newco = test_version_5()
+    newco = test_version_6()
     print("All tests passed")
     
+
+
     
 
      
