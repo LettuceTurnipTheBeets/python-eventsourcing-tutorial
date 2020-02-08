2a4f3215f635879555e96e23f8d3c551d136f6fd
Zarek <zarek@zarekr.com>
2020-02-08 14:45:36 +0000

    moved command work out of events

--- part_3-application_layer/domain_model.py
+++ part_3-application_layer/domain_model.py
@@ -69,6 +69,9 @@ class Company(AggregateRoot):
     def name(self):
         pass
 
+    class Event(AggregateRoot.Event):
+        pass
+
     def create_share_class(
             self,
             name: str,
@@ -78,27 +81,19 @@ class Company(AggregateRoot):
             votes_per_share: int = 1,
             redeemable: bool = False
         ) -> None: 
-            self.__trigger_event__(
-                Company.ShareClassWasCreated,
-                name=name,
-                nominal_value=nominal_value,
+            new_share_class = ShareClass(
+                name=name, 
+                nominal_value=nominal_value, 
                 entitled_to_dividends=entitled_to_dividends,
                 entitled_to_capital=entitled_to_capital,
                 votes_per_share=votes_per_share,
                 redeemable=redeemable
             )
-
-    class Event(AggregateRoot.Event):
-        pass
+            self.__trigger_event__(Company.ShareClassWasCreated, new_share_class=new_share_class)
 
     class ShareClassWasCreated(Event):
         def mutate(event, company):
-            new_share_class = ShareClass(
-                name=event.name, 
-                nominal_value=event.nominal_value, 
-                votes_per_share=event.votes_per_share
-            )
-            company.share_classes[new_share_class.name] = new_share_class
+            company.share_classes[event.new_share_class.name] = event.new_share_class
 
     def allot_shares(
         self,
@@ -110,95 +105,57 @@ class Company(AggregateRoot):
             raise ShareClassError(f"Expected <class 'ShareClass'>: got {type(share_class)}")
         if share_class.name not in self.share_classes:
             raise ShareClassError(f"No such share class: {share_class.name}")
-        self.__trigger_event__(
-            Company.NewSharesAllotted,
-            legal_person=legal_person,
-            share_class=share_class,
-            number_of_shares=number_of_shares
-        )
+        new_shares = Shares(number=number_of_shares, share_class=share_class, in_company=self)
+        new_shareholding = Shareholding(legal_person=legal_person, shares=new_shares, status="ALLOTTED")
+        self.__trigger_event__(Company.NewSharesAllotted, new_shareholding=new_shareholding)
 
     class NewSharesAllotted(Event):
         def mutate(event, company):
-            new_shares = Shares(
-                number=event.number_of_shares, 
-                share_class=event.share_class,
-                in_company=company
-            )
-            new_shareholder = Shareholder(
-                legal_person=event.legal_person,
-                shares=new_shares,
-                status="ALLOTTED"
-            )
-            company.allotted.append(new_shareholder)
+            company.allotted.append(event.new_shareholding)
 
-    def get_allotments(self, to_person: Person = None) -> List:
-        if not to_person:
-            return self.allotted
-        return [a for a in self.allotted if a.legal_person == to_person] 
-            
     def issue_shares(
         self, 
-        to_person: Shareholder, 
-        issue_date: Decimal = None 
+        to_person: Shareholding, 
+        issue_date: Decimal = None
     ) -> None:
-        allotments = self.get_allotments(to_person)
+        allotments = [a for a in self.allotted if a.legal_person == to_person] 
         if not allotments: 
-            raise ShareholderError(f"No shares allotted to {to_person}")
-        self.__trigger_event__(
-            Company.SharesIssued, 
-            shares_to_issue=allotments, 
-            number_of_issuances=len(allotments),
-            issue_date=issue_date
-        )
+            raise ShareholdingError(f"No shares allotted to {to_person}")
+        self.__trigger_event__(Company.SharesIssued, shares_to_issue=allotments)
 
-    def issue_all_allotted_shares(
+    def issue_all_shares(
         self, 
         issue_date: Decimal = None
     ) -> None: 
-        self.__trigger_event__(
-            Company.SharesIssued, 
-            shares_to_issue=self.allotted,
-            number_of_issuances=len(self.allotted),
-            issue_date=issue_date
-        )
+        if not self.allotted: 
+            raise ShareholdingError(f"No shares allotted")
+        allotments = self.allotted.copy()
+        self.__trigger_event__(Company.SharesIssued, shares_to_issue=allotments)
 
     class SharesIssued(Event):
-        def mutate(event, company): 
-            print("allotted before issue", len(company.allotted))
-            allot_now = event.shares_to_issue.copy()
-            company.allotted = [shares for shares in company.allotted if shares not in allot_now]
-            while allot_now:
-                company.issued.append(allot_now.pop())
-            # try:
-                # if called outside of the try/except
-                # this fails with ValueError: list.remove(x): x not in list
-                # for s in event.shares_to_issue:
-                #     company.issued.append(s)
-                #     company.allotted.remove(s)
-            # except ValueError:
-            #     pass
-            print("allotted after issue", len(company.allotted))
+        def mutate(event, company):
+            for shareholding in event.shares_to_issue:
+                company.allotted.remove(shareholding)
+                company.issued.append(shareholding)
 
-    def get_share_class_data(self, share_class: ShareClass) -> dict:
-        if share_class.name not in self.share_classes:
-            raise ShareClassError(f"{self.name} has no share class: {share_class}")
+    # TODO: need to rewrite this
+    # def get_share_class_data(self, share_class: ShareClass) -> dict:
+    #     if share_class.name not in self.share_classes:
+    #         raise ShareClassError(f"{self.name} has no share class: {share_class}")
+    #     total_number_of_shares = 0
+    #     aggregate_nominal_value = 0
+    #     for sh in self.issued:
+    #         for holding in sh.shareholding:
+    #             if holding.share_class == share_class:
+    #                 total_number_of_shares += holding.number
+    #                 aggregate_nominal_value += (holding.number * holding.share_class.nominal_value)
+    #     return dict(
+    #         share_class = share_class.name,
+    #         total_number_of_shares = total_number_of_shares, 
+    #         aggregate_nominal_value = aggregate_nominal_value,
+    #     )
 
-        total_number_of_shares = 0
-        aggregate_nominal_value = 0
-        # need to rewrite this
-        for sh in self.issued:
-            for holding in sh.shareholding:
-                if holding.share_class == share_class:
-                    total_number_of_shares += holding.number
-                    aggregate_nominal_value += (holding.number * holding.share_class.nominal_value)
-
-        return dict(
-            share_class = share_class.name,
-            total_number_of_shares = total_number_of_shares, 
-            aggregate_nominal_value = aggregate_nominal_value,
-        )
-
-class Shareholder(object):
+class Shareholding(object):
     def __init__(
         self, 
         legal_person: Person, 
@@ -210,7 +167,7 @@ class Shareholder(object):
         self.status = status
 
     def __repr__(self):
-        return f"<Shareholder(legal_person={self.legal_person}, shares={self.shareholding}, status='{self.status}')>"
+        return f"<Shareholding(legal_person={self.legal_person}, shares={self.shareholding}, status='{self.status}')>"
 
 class ShareClass(object):
     def __init__(
@@ -261,7 +218,7 @@ class Shares(object):
 class ShareClassError(Exception):
     pass
 
-class ShareholderError(Exception):
+class ShareholdingError(Exception):
     pass
 
 # quick tests
@@ -278,42 +235,15 @@ if __name__ == "__main__":
 
     # set up
     jack = Person.__create__(name="Jack")
-    vic = Person.__create__(name="Vic")
-    hood = Person.__create__(name="Hood")
-
-    for person in [jack, vic, hood]:
-        person.__save__()
+    jack.__save__()
     
     company = Company.__create__(name="Company Ltd")
     company.create_share_class(name="ordinary", nominal_value=0.001)    
-    company.create_share_class(name="preferred", nominal_value=0.001)
     ord = company.share_classes["ordinary"]
-    pref = company.share_classes["preferred"]
 
-    # allot 2 sets of shares to jack (one pref, one ord)
     company.allot_shares(legal_person=jack, share_class=ord, number_of_shares=100)
-    company.allot_shares(legal_person=jack, share_class=pref, number_of_shares=10120)
-    # allot 2 more sets of ord shares to the others
-    company.allot_shares(legal_person=vic, share_class=ord, number_of_shares=100)
-    company.allot_shares(legal_person=hood, share_class=ord, number_of_shares=100)
-    
-    # check that all of the above are stored in company.allotted
-    assert len(company.allotted) == 4
-    # check that 2 of the above are for jack only
-    assert len(company.get_allotments(jack)) == 2    
+    company.allot_shares(legal_person=jack, share_class=ord, number_of_shares=251)
+    company.allot_shares(legal_person=jack, share_class=ord, number_of_shares=29151)
 
-    # issue the shares to jack only
-    company.issue_shares(jack)
+    # company.issue_shares(jack)
     company.__save__()
-
-    # check that only 2 sets of shares have been issued
-    assert len(company.issued) == 2
-    # check that no more shares should be issued to jack
-    assert len(company.get_allotments(jack)) == 0
-    # check that 2 sets of shares remain in company.allotted
-    assert len(company.allotted) == 2
-
-    company.issue_all_allotted_shares()
-    company.__save__()
-    assert len(company.allotted) == 0
-    assert len(company.issued) == 4
